ecd017a8eafe515d393c645b1ac7d9f7a28c51b8
Agathe_Puissant <agacarpu2@hotmail.fr>
2019-10-02 19:24:52 +0200

    Update code_base.py

--- code_base.py
+++ code_base.py
@@ -8,10 +8,11 @@ import random
 
 class Entity(object):
     
-    def __init__(self, _proba_connection=random(), _proba_consulter=random(), _proba_accepter=random(),
+    def __init__(self, _nom, _proba_connection=random(), _proba_consulter=random(), _proba_accepter=random(),
                  _proba_transmettre=random()):
         
-        self.voisin = list()
+        self.voisin = list() #stocke le nom des voisins de l'entité (?)
+        self.nom='' #rajoute le numéro de l'entité pour pouvoir les repérer dans le réseau
         self.proba_connection = _proba_connection
         self.proba_consulter = _proba_consulter
         self.proba_accepter = _proba_accepter
@@ -38,7 +39,7 @@ class Reseau(object):
     def __init__(self):
         self.entities = list()
     
-    def initialiser_reseau(self, _nb_entities):
+    def initialiser_reseau(self, _nb_bon_public,_proba_bon_public,_nb_mauvais_public,_proba_mauvais_public):
         """
         initialiser le nombre d'entités
         appeller la méthode initialiser_Public pour gérer les publics au sein
@@ -48,8 +49,11 @@ class Reseau(object):
 
         """
         
-        self.entities+=[Entity() for i in range(_nb_entities)]
-        
+        self.entities=self.initialiser_public(_nb_bon_public,_proba_bon_public)+self.initialiser_public(_nb_mauvais_public,_proba_mauvais_public)
+       
+        for i in range(len(self.entities)) :
+            self.entities[i].nom=str(i) #défini le nom de chaque entité (utile dans calculer distance)
+    
     
     def initialiser_public(self, _nb_entities, _proba_accept):
         """
@@ -59,14 +63,46 @@ class Reseau(object):
         -->ajoute des entités à l'attribut *entities* de la classe
         """
         
-        self.entities+=[Entity(_proba_accepter=_proba_accept) for i in range(_nb_entities)]
+        return [Entity(_proba_accepter=_proba_accept) for i in range(_nb_entities)]
     
-    def calculer_distance(self,_entity_A, entity_B):
+    def calculer_distance(self, _entity_A, _entity_B, path=[]):
         """
         probablement implementer Dijkstra ou un autre algo de calcul de "plus
         court chemain"
         """
-    
+        
+        #fonction contruite à partir de https://www.python.org/doc/essays/graphs/
+        
+        graph={} #on crée un dictionnaire qui stocke en clé le nom de chaque entité et dans les valeurs le nom des voisins de chaque entité
+        
+        for i in range(len(self.entities)) :
+            graph[self.entities[i].nom]=self.entities[i].voisin
+            
+        path=path+[_entity_A] #fonction récursive donc le chemin s'update à chaque nouveau "tour"
+        
+        if _entity_A==_entity_B : #fin du chemin
+            return path 
+        
+        if not graph.has_key(_entity_A) : #pas de chemin possible
+            return None
+        
+        shortest=None  #on initialise le plus court chemin à None
+        
+        for node in graph[_entity_A]: #pour chaque voisin de l'entité A
+            
+            if node not in path : #si on est pas déjà passé par ce voisin
+                newpath=self.calculer_distance(node,_entity_B,path) #on relance le calcul du chemin à partir de ce voisin
+                
+                if newpath : #si le nouveau chemin n'est pas None donc est possible
+                    if not shortest or len(newpath)<len(shortest) : #si le plus court chemin existe déjà ou si le nouveau chemin est plus court que l'ancien plus court
+                        shortest=newpath #le plus court chemin est update
+                        
+        return len(shortest)
+        
+        
+        
+        
+        
     def calculer_diametre(self):
         """
         réutiliser *calculer_distance* entre chaque couples d'entité et retourner
